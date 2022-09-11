87a90b55bdf25ab1eb8b8018c3d77065ccf3f3d0
Richard Rodrigues da Silva <elaine.ra96@gmail.com>
2022-09-11 15:26:06 -0300

    Complementando a Toolbar, adicionando links e e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(h => h.HomeModule) },
+  { path: 'explore', loadChildren: () => import('./pages/explore/explore.module').then(e => e.ExploreModule) },
+  { path: 'watch', loadChildren: () => import('./pages/watch/watch.module').then(w => w.WatchModule) },
+  { path: 'play', loadChildren: () => import('./pages/play/play.module').then(p => p.PlayModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/watch/watch-routing.module.ts
+++ src/app/pages/watch/watch-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WatchComponent } from './watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: WatchComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
