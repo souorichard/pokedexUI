12d01acaf699f0c87c9c4e4439e5d2926a42c48d
Richard Rodrigues da Silva <elaine.ra96@gmail.com>
2022-09-16 18:56:06 -0300

    Arrumando Erros no Componente Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: '', loadChildren: () => import('./pages/home/home.module').then(h => h.HomeModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(h => h.HomeModule) },
   { path: 'explore', loadChildren: () => import('./pages/explore/explore.module').then(e => e.ExploreModule) },
   { path: 'watch', loadChildren: () => import('./pages/watch/watch.module').then(w => w.WatchModule) },
   { path: 'play', loadChildren: () => import('./pages/play/play.module').then(p => p.PlayModule) },
