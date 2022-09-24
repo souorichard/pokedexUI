c932740b7857814cfa5480602ddec9d0971dbd15
Richard Rodrigues da Silva <elaine.ra96@gmail.com>
2022-09-24 20:40:44 -0300

    Arrumando erro do componente Pokemon-List

--- src/app/models/LoadMore.ts
+++ src/app/models/LoadMore.ts
@@ -1,5 +1,2 @@
 export function loadMore() {
-  const valInitial = 0;
-
-  return valInitial;
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,22 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pokemon } from '../models/Pokemon';
 import { from, map, mergeMap } from 'rxjs';
-import { loadMore } from '../models/LoadMore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  public loadMore = loadMore;
-
   public pokemons: Pokemon[] = [];
 
-  allPokemonsUrl: any = `https://pokeapi.co/api/v2/pokemon/?limit=12&offset=${loadMore()}`;
+  allPokemonsUrl: any = `https://pokeapi.co/api/v2/pokemon/?limit=12`;
 
   constructor( private httpClient: HttpClient ) {
 
-    this.httpClient.get<any>(this.allPokemonsUrl).pipe(
+    this.httpClient.get<any>(this.allPokemonsUrl)
+    .pipe(
       map(value => value.results),
       map((value: any) => {
         return from(value).pipe(
@@ -25,7 +23,8 @@ export class PokemonService {
         );
       }),
       mergeMap(value => value)
-    ).subscribe((result: any) => this.pokemons[result.id] = {
+    )
+    .subscribe((result: any) => this.pokemons[result.id] = {
       image: result.sprites.front_default,
       number: result.id,
       name: result.name,
